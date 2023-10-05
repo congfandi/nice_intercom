f6d44a14c861597756a18031863052f6e5cf650e
congfandi <congfandi@gmail.com>
2023-10-05 09:41:36 +0700

    Finish implementasi on ios

--- ios/Classes/NiceIntercomPlugin.swift
+++ ios/Classes/NiceIntercomPlugin.swift
@@ -1,19 +1,44 @@
 import Flutter
 import UIKit
 
-public class NiceIntercomPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "nice_intercom", binaryMessenger: registrar.messenger())
-    let instance = NiceIntercomPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+class NiceIntercomPlugin: NSObject, FlutterPlugin, MethodCallHandler {
+    private var intercom: Intercom?
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    func handle(call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "initialize":
+            guard let args = call.arguments as? [String: Any] else {
+                result(FlutterError(code: "invalidArguments", message: "Invalid arguments", details: nil))
+                return
+            }
+
+            let appId = args["appId"] as! String
+            let apiKey = args["apiKey"] as! String
+
+            intercom = Intercom(appId: appId, apiKey: apiKey)
+            intercom?.start()
+
+            result(nil)
+        case "register":
+            guard let args = call.arguments as? [String: Any] else {
+                result(FlutterError(code: "invalidArguments", message: "Invalid arguments", details: nil))
+                return
+            }
+
+            let userId = args["userId"] as! String
+            let email = args["email"] as? String
+
+            let registration = Registration(userId: userId, email: email)
+            intercom?.register(registration: registration)
+
+            result(nil)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
     }
-  }
+
+    func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "com.thengoding.nice_intercom", binaryMessenger: registrar.messenger())
+        channel.setMethodCallHandler(self)
+    }
 }
